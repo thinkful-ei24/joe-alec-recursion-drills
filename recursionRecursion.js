bcf84c865ae05450d31351c7cc08931df8e2ef90
Joe-Pena <joepena@protonmail.com>
2018-11-05 10:08:21 -0700

    Reverse String

--- recursionRecursion.js
+++ recursionRecursion.js
@@ -10,8 +10,25 @@ function sheepCount(numOfSheep) {
 sheepCount(5);
 
 //#2 ARRAY NUMBER DOUBLER
-function arrayDoubler(list) {
+// function arrayDoubler(list) {
+//   if(list.length === 0) {
+//     return [];
+//   }
+//   let newNumber = list.shift() * 2;
+//   list.
+//   console.log(list);
+//   arrayDoubler(list);
+// }
 
+// console.log(arrayDoubler([1, 2, 3, 4, 5]));
+
+//#3 REVERSE STRING 
+function reverseStr(str) {
+  if(str.length === 0) {
+    return str;
+  }
+
+  return str.slice(-1) + reverseStr(str.slice(0, str.length - 1));
 }
 
-console.log(arrayDoubler([1, 2, 3, 4, 5]));
+console.log(reverseStr('Hello'));
